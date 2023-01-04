032857eb32617e56333756ab9e87d27eebff28bf
Hashim Muqtadir <hashim.muqtadir@hivery.com>
2023-01-04 12:13:42 +1100

    Add day12 part 2 and refactor a little

--- day12.rkt
+++ day12.rkt
@@ -17,36 +17,35 @@
 
 (struct pt (x y) #:transparent)
 
-(define (possible-moves grid visited x y)
-  (define cur-height (->elevation (grid-ref grid x y)))
-  (define down (pt x (add1 y)))
-  (define up (pt x (sub1 y)))
-  (define left (pt (sub1 x) y))
-  (define right (pt (add1 x) y))
-  (for/list ([move (list down up left right)]
-             #:when (and (within-limits grid (pt-x move) (pt-y move))
-                         (<= (->elevation (grid-ref grid (pt-x move) (pt-y move))) (add1 cur-height))
-                         (not (set-member? visited move))))
-    move))
+(define (candidates x y) (list (pt x (add1 y)) (pt x (sub1 y)) (pt (sub1 x) y) (pt (add1 x) y)))
 
-(define (search grid)
+(define (search grid (start-label #\S) (end-label #\E) (step-fn (λ(n c) (<= n (add1 c)))))
+  (define (possible-moves x y)
+    (define possibilities (candidates x y))
+    (define cur-height (->elevation (grid-ref grid x y)))
+    (for/list ([move possibilities]
+               #:when (and (within-limits grid (pt-x move) (pt-y move))
+                           (step-fn (->elevation (grid-ref grid (pt-x move) (pt-y move))) cur-height)
+                           (not (set-member? visited move))))
+      move))
   (define visited (mutable-set))
   (match-define (pt start-x start-y)
     (for*/last ([(row y) (in-indexed grid)] [(height-label x) (in-indexed row)])
-      #:final (equal? height-label #\S)
+      #:final (equal? height-label start-label)
       (pt x y)))
   (set-add! visited (pt start-x start-y))
   (let loop ([cur-posns (list (pt start-x start-y))]
              [step-num 1])
     (define next-moves
       (remove-duplicates
-       (append-map (λ(p) (possible-moves grid visited (pt-x p) (pt-y p)))
+       (append-map (λ(p) (possible-moves (pt-x p) (pt-y p)))
                    cur-posns)))
     (for ([p next-moves]) (set-add! visited p))
-    (if (member #\E (map (λ(p) (grid-ref grid (pt-x p) (pt-y p))) next-moves))
+    (if (member end-label (map (λ(p) (grid-ref grid (pt-x p) (pt-y p))) next-moves))
         step-num
         (loop next-moves
               (add1 step-num)))))
 
 (module+ main
-  (search (read-inp)) #;497)
+  (search (read-inp)) #;497
+  (search (read-inp) #\E #\a (λ(n c) (>= n (sub1 c)))) #;492 )
